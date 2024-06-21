b21a9096761dace418fd9dd2e40b3991375daa10
Namada Junior <namada871@gmail.com>
2024-06-21 13:10:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+    name: 'John Doe',
+    role: 'Software Engineer',
+    salary: 50000
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const clone = { ...employee };
+    delete clone[key];
+    return clone;
+  }
+  
+  // Destructive delete (modifies the original object)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
